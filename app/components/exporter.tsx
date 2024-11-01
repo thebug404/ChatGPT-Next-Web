fe0f3567ca4004c053ea6230afc3657377b5d4d6
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-10-31 19:42:18 -0600

    chore: Update application references from "NextChat" to "Brainy"

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -539,10 +539,10 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
-            <div className={styles["sub-title"]}>
+            <div className={styles["main-title"]}>Brainy</div>
+            {/* <div className={styles["sub-title"]}>
               github.com/ChatGPTNextWeb/ChatGPT-Next-Web
-            </div>
+            </div> */}
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
               <span className={styles["icon-space"]}>&</span>
