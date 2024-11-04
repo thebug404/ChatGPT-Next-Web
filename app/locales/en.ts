b6495196602ec335b353aefd6b7519e25606e624
Ivan Guevara <ivanzaldivar16@gmail.com>
2024-11-03 21:37:50 -0600

    fix: update error messages for unauthorized access in English and Spanish locales

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -1,21 +1,25 @@
 import { getClientConfig } from "../config/client";
 import { SubmitKey } from "../store/config";
 import { LocaleType } from "./index";
-import { SAAS_CHAT_UTM_URL } from "@/app/constant";
 // if you are adding a new translation, please use PartialLocaleType instead of LocaleType
 
 const isApp = !!getClientConfig()?.isApp;
 const en: LocaleType = {
   WIP: "Coming Soon...",
   Error: {
+    // Unauthorized: isApp
+    //   ? `😆 Oops, there's an issue. No worries:
+    //  \\ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
+    //  \\ 2️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️`
+    //   : `😆 Oops, there's an issue. Let's fix it:
+    //  \ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
+    //  \ 2️⃣ Using a private setup? [Click here](/#/auth) to enter your key 🔑
+    //  \ 3️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️
+    //  `,
     Unauthorized: isApp
-      ? `😆 Oops, there's an issue. No worries:
-     \\ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \\ 2️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️`
-      : `😆 Oops, there's an issue. Let's fix it:
-     \ 1️⃣ New here? [Click to start chatting now 🚀](${SAAS_CHAT_UTM_URL})
-     \ 2️⃣ Using a private setup? [Click here](/#/auth) to enter your key 🔑
-     \ 3️⃣ Want to use your own OpenAI resources? [Click here](/#/settings) to change settings ⚙️
+      ? `😥 Oops, there's an issue.`
+      : `😥 Oops, there's an issue. Let's fix it:
+     \ 1️⃣ [Click here](/#/auth) to enter your key 🔑
      `,
   },
   Auth: {
